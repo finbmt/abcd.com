a1533089ef249bb45833db17d94edb046a340c26
finbmt <47vn.fin@gmail.com>
2016-08-30 23:48:43 +0700

    read

--- gulpfile.js
+++ gulpfile.js
@@ -6,11 +6,26 @@ var elixir = require('laravel-elixir');
  |--------------------------------------------------------------------------
  |
  | Elixir provides a clean, fluent API for defining some basic Gulp tasks
- | for your Laravel application. By default, we are compiling the Sass
+ | for your Laravel application. By default, we are compiling the Less
  | file for our application, as well as publishing vendor resources.
  |
  */
 
 elixir(function(mix) {
-    mix.sass('app.scss');
+
+    mix.sass([
+		'app.scss'
+    ], 'public/css/app.css');
+
+    mix.scripts([
+		'alerts.js',
+        'dialogs.js',
+        'spinners.js'
+    ], 'public/js/app.js');
+
+    mix.version([
+    	'css/app.css',
+    	'js/app.js'
+    ]);
+
 });
